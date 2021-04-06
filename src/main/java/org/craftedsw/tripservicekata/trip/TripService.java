3b64df33e8c5dd2142aa554efdf7835bf09c3b74
atorres <atorres@redpoints.com>
2021-04-06 14:32:41 +0200

    Testing UserNotLoggedInException

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,32 +1,35 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
+	public List<Trip> getTripsByUser(final User user) throws UserNotLoggedInException {
+		final User loggedUser = this.getSession().getLoggedUser();
+		if (loggedUser == null) {
 			throw new UserNotLoggedInException();
 		}
+		List<Trip> tripList = new ArrayList<>();
+		boolean isFriend = false;
+		for (final User friend : user.getFriends()) {
+			if (friend.equals(loggedUser)) {
+				isFriend = true;
+				break;
+			}
+		}
+		if (isFriend) {
+			tripList = TripDAO.findTripsByUser(user);
+		}
+		return tripList;
 	}
-	
+
+	protected UserSession getSession() {
+		return UserSession.getInstance();
+	}
+
 }
--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,29 @@
 package org.craftedsw.tripservicekata.trip;
 
-public class TripServiceTest {
-	
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserSession;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+class TripServiceTest {
+
+	@Test
+	void GivenNotUserLoggedIn_WhenGetTripsByUser_ThenShouldRaiseException() {
+		// Given
+		final UserSession userSession = Mockito.mock(UserSession.class);
+		given(userSession.getLoggedUser())
+				.willReturn(null);
+
+		final TripService tripService = Mockito.spy(TripService.class);
+		given(tripService.getSession())
+				.willReturn(userSession);
+
+		assertThrows(UserNotLoggedInException.class, () ->
+				tripService.getTripsByUser(new User())
+		);
+	}
 }
